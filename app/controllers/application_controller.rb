3277da561411d5d16517872f151a621a7cc1a157
Sean Cotton <sean@seancotton.com>
2017-05-04 20:38:08 -0400

    fixed some bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :categories, :brands
+
+  def categories
+  	@categories = Category.all
+  	
+  end
+
+  def brands
+    @brands = Product.pluck(:brand).sort.uniq
+  end
+ 
 end
